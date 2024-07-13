23ab62de8ace53d27f83c184fa0f41e95bfd5c5b
CharanReddy404 <Charanlucky6143@gmail.com>
2024-07-13 23:03:59 +0530

    feat(1): repo setup of eslint, prettier and husky

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '*' },
+      { protocol: 'http', hostname: '*' },
+    ],
+  },
+};
 
 export default nextConfig;
