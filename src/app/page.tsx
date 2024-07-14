2727b4a223b1f1be69aa232575cae278269cc4e0
CharanReddy404 <Charanlucky6143@gmail.com>
2024-07-14 20:00:50 +0530

    ci: testing ci commit-msg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1 className='mb-8 text-center text-6xl font-bold'>MakeDevEasy</h1>
 
       <p className='text-center text-xl text-gray-600'>
-        We are currently under development and will be launching soon!!!!
+        We are currently under development and will be launching soon!
       </p>
 
       <p className='mt-8 text-center text-gray-500'>Stay tuned for exciting updates!</p>
