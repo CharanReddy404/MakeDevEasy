bc211094fc9935f2121eb97d62346b3e990d8591
charanreddy404 <charanlucky6143@gmail.com>
2024-07-21 13:05:29 +0530

    feat(Header): implimented header and added discord link

---
+++ eslint.config.ts
@@ -0,0 +1,80 @@
+// .eslintrc.js
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  root: true,
+  extends: [
+    'next',
+    'eslint:recommended',
+    'prettier',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    // JavaScript rules
+    'prefer-const': 'warn',
+    'no-var': 'warn',
+    'no-unused-vars': 'warn',
+    'object-shorthand': 'warn',
+    'quote-props': ['warn', 'as-needed'],
+    // TypeScript rules
+    '@typescript-eslint/array-type': [
+      'warn',
+      {
+        default: 'array',
+      },
+    ],
+    '@typescript-eslint/consistent-type-assertions': [
+      'warn',
+      {
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'never',
+      },
+    ],
+    // React rules
+    'react/jsx-fragments': ['warn', 'syntax'], // Shorthand syntax for React fragments
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['ts', 'tsx'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: [
+    '.next',
+    '.cache',
+    'package-lock.json',
+    'public',
+    'node_modules',
+    'next-env.d.ts',
+    'next.config.ts',
+    'yarn.lock',
+    'pnpm-lock.yaml',
+    'src/components/ui',
+  ],
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { ModeToggle } from './ModeToggle';
+
+export default function Header() {
+  return (
+    <header className='fixed flex w-screen items-center justify-between border-b bg-background px-4 py-3'>
+      <Link href='#' className='text-lg font-bold' prefetch={false}>
+        MakeDevEasy
+      </Link>
+      <div className='flex items-center gap-4'>
+        <Button variant='secondary' size='sm' className='rounded-md'>
+          <Link href='https://discord.gg/VdNZZpD2ey' target='_blank'>
+            Join us on Discord
+          </Link>
+        </Button>
+        <ModeToggle />
+      </div>
+    </header>
+  );
+}
